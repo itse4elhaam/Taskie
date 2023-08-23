a5fa5ea6121626fa2a93db62adf6b78396b5121d
Elhaam <e4elhaam@gmail.com>
2023-08-23 11:23:14 +0500

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// we can properties here that can be used in the component.html file
 export class AppComponent {
-  title = 'task-tracker';
 }
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input() text!: string;
+  // this is a custom event and can be referenced anywhere we use the app-button
+  @Output() btnClick = new EventEmitter();
+
+  onClick(){
+    this.btnClick.emit();
+  }
+}
